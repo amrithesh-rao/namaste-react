5931219c2231f7d77d761edab9e05963b50cf66c
Amrithesh Rao <amritheshatwork@gmail.com>
2024-08-25 03:43:30 -0700

    completed assignment 2

---
+++ Chapter 02 - Igniting app/Code/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const ele = React.createElement("div", { id: "parent"},
+    React.createElement(
+        "div", 
+        { id: "child1"},
+        [
+           React.createElement("h2", {}, "first"),
+           React.createElement("h2", {}, "second") 
+        ]
+    )
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(ele);
