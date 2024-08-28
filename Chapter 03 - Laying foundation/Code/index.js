1c4da6208766e70a1d190a185f574613ba2aa3f1
Amrithesh Rao <amritheshatwork@gmail.com>
2024-08-28 12:08:44 -0700

    coding added

--- Chapter 03 - Laying foundation/Code/index.js
+++ Chapter 03 - Laying foundation/Code/index.js
@@ -1,5 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import logo from "./img/logo.png";
+import pc from "./img/pc.png";
+import "./style.css";
 
 // jsx
 const heading = <h1 className="head"> journey of a developer</h1>;
@@ -34,5 +37,49 @@ const Compo4 = () => (
   </div>
 );
 
+// assign1
+const testHeader = React.createElement("div", { className: "title" }, [
+  React.createElement("h1", {}, "head1"),
+  React.createElement("h2", {}, "head2"),
+  React.createElement("h3", {}, "head3"),
+]);
+const testHeader2 = (
+  <div className="title">
+    <h1>head1</h1>
+    <h2>head2</h2>
+    <h3>head3</h3>
+  </div>
+);
+
+const Title = () => (
+  <div className="title">
+    <h1 style={{ color: "red" }}>head1</h1>
+    <h2 style={{ color: "yellow" }}>head2</h2>
+    <h3>head3</h3>
+    <AnotherComponent />
+  </div>
+);
+
+const AnotherComponent = () => <h3>Another component</h3>;
+
+//assign2
+const HeaderComponent = () => (
+  <div className="container">
+    <div>
+      <img src={logo} />
+    </div>
+    <div className="center">
+      <input type="text" placeholder="Type anything.." />
+    </div>
+    <div>
+      <img src={pc} />
+    </div>
+  </div>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Compo4 />);
+// root.render(<Compo4 />);
+// root.render(testHeader2);
+// root.render(<Title></Title>);
+root.render(<HeaderComponent />);
+// root.render(Title());
