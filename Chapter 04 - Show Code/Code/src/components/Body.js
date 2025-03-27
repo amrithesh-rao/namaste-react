20e24d74f36e9656a12709691e81a067b121b3dc
Amrithesh Rao <amritheshatwork@gmail.com>
2025-03-28 01:35:44 +0530

    added search functionality and taking data from realtime api

--- Chapter 04 - Show Code/Code/src/components/Body.js
+++ Chapter 04 - Show Code/Code/src/components/Body.js
@@ -1,21 +1,57 @@
-import { restData } from "../utils/mockData";
 import RestorantCard from "./RestaurantCard";
 import { useEffect, useState } from "react";
 
 const Body = () => {
-  const [restDetail, setRestDetail] = useState(restData);
+  const [restDetail, setRestDetail] = useState([]);
+  const [filteredRestDetail, setFilteredRestDetail] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
 
-  useEffect(()=>{
-      fetch(`https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&page_type=DESKTOP_WEB_LISTING`).then((response)=>{
-        response.json().then((res)=>{
+  useEffect(() => {
+    fetch(
+      `https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&page_type=DESKTOP_WEB_LISTING`
+    )
+      .then((response) => {
+        response.json().then((res) => {
           console.log(res);
-        })
-      }).catch((err)=>{
-        console.log(err);
+          console.log(
+            res?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle
+              ?.restaurants
+          );
+          setRestDetail(
+            res?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle
+              ?.restaurants
+          );
+          setFilteredRestDetail(res?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle
+            ?.restaurants);
+        });
       })
+      .catch((err) => {
+        console.log(err);
+      });
   }, []);
-  return (
+  return restDetail.length === 0 ? (
+    "loading"
+  ) : (
     <div className="food-body">
+      <input
+        id="rest-search-button"
+        placeholder="type restorant name here"
+        value={searchValue}
+        onChange={(e) => {
+          setSearchValue(e.target.value);
+        }}
+      />
+      <button onClick={() => {
+        if (searchValue.length === 0) {
+          setFilteredRestDetail(restDetail);
+          return;
+        }
+        const filterData = restDetail.filter(restItem=>
+          restItem.info.name.toLowerCase().split(' ').includes(searchValue.toLowerCase())
+        );
+        console.log(filterData);
+        setFilteredRestDetail(filterData);
+      }}>Go</button>
       <button
         onClick={() => {
           const filterData = restDetail.filter(
@@ -30,7 +66,7 @@ const Body = () => {
       {/* <RestorantSearchBarComponent /> */}
       <div className="card-container">
         {/*this is a comment*/}
-        {restDetail.map((restItem) => (
+        {filteredRestDetail.map((restItem) => (
           <RestorantCard key={restItem.info.id} restData={restItem} />
         ))}
       </div>
