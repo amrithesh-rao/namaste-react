ce96d63dd64fd36050f2aa9f3a3fee4fb5cb6c57
Amrithesh Rao <amritheshatwork@gmail.com>
2025-04-04 00:33:21 +0530

    creating browserroute in app

---
+++ Chapter 04 - Show Code/Code/src/components/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      Its about my food app.
+    </div>
+  )
+}
+
+export default About;
--- Chapter 04 - Show Code/Code/src/index.js
+++ Chapter 04 - Show Code/Code/src/index.js
@@ -2,6 +2,9 @@ import ReactDOM from "react-dom/client";
 import React from "react";
 import Header from "./components/Header";
 import Body from "./components/Body";
+import About from "./components/About";
+import ContactUs from "./components/ContectUs";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const FoodApp = () => (
   <div className="food-app">
@@ -10,6 +13,21 @@ const FoodApp = () => (
   </div>
 );
 
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <FoodApp />
+  },
+  {
+    path: "/about",
+    element: <About />
+  },
+  {
+    path: "/contact",
+    element: <ContactUs />
+  }
+]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<FoodApp />);
+root.render(<RouterProvider router={appRouter} />);
